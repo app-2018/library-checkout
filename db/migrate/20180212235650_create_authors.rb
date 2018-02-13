378e9e9d6bd19b5af4b609c0ab286e604316f3db
Karl Stolley <karl.stolley@gmail.com>
2018-02-12 18:02:39 -0600

    Add Author first name to migration

--- db/migrate/20180212235650_create_authors.rb
+++ db/migrate/20180212235650_create_authors.rb
@@ -2,6 +2,7 @@ class CreateAuthors < ActiveRecord::Migration[5.1]
   def change
     create_table :authors do |t|
       t.string :last_name
+      t.string :first_name
 
       t.timestamps
     end
