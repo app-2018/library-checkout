6f87617a4fba355bcdf5f427bc5fa05ebb86bd63
Karl Stolley <karl.stolley@gmail.com>
2018-02-12 18:14:10 -0600

    Run Author migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180212234824) do
+ActiveRecord::Schema.define(version: 20180212235650) do
+
+  create_table "authors", force: :cascade do |t|
+    t.string "last_name"
+    t.string "first_name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "books", force: :cascade do |t|
     t.string "title"
