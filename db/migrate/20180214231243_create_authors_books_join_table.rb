85664a1089078c82f9b25591b85eac2fd62e6af9
Karl Stolley <karl.stolley@gmail.com>
2018-02-14 17:21:34 -0600

    Write migration to create authors-books join table

--- db/migrate/20180214231243_create_authors_books_join_table.rb
+++ db/migrate/20180214231243_create_authors_books_join_table.rb
@@ -1,4 +1,8 @@
 class CreateAuthorsBooksJoinTable < ActiveRecord::Migration[5.1]
   def change
+    create_table :authors_books, id: false do |t|
+      t.belongs_to :author, index: true
+      t.belongs_to :book, index: true
+    end
   end
 end
