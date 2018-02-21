88db855d24d2eceefd83cb53e4169e15beac50dd
Karl Stolley <karl.stolley@gmail.com>
2018-02-21 17:31:16 -0600

    Ensure that book records have titles

--- test/models/book_test.rb
+++ test/models/book_test.rb
@@ -1,7 +1,8 @@
 require 'test_helper'
 
 class BookTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should not save book without a title" do
+    book = Book.new
+    assert_not book.save, "Saved a book without a title"
+  end
 end
