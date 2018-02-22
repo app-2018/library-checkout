7272babf1f62411c48ef008345e0da4d5e88ebf2
Karl Stolley <karl.stolley@gmail.com>
2018-02-21 18:14:18 -0600

    Validate presence and uniqueness of User email

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
 class User < ApplicationRecord
   belongs_to :role
+  validates :email, presence: true, uniqueness: true
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,4 +4,10 @@ class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  test "user email address must be unique" do
+    user = User.new
+    user.role = roles(:patron)
+    user.email = users(:scarlet).email
+    assert_not user.save, "Saved a duplicate email address"
+  end
 end
