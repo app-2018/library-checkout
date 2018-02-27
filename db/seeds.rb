92d657a64302497e94ff1120f42e6c562c5dd70b
Karl Stolley <karl.stolley@gmail.com>
2018-02-26 18:11:26 -0600

    Begin building seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Book.destroy_all
+Author.destroy_all
+Role.destroy_all
+User.destroy_all
+
+@author = Author.create(last_name: "Wallace", first_name: "David Foster")
+
+@book = Book.create(title: "Infinite Jest", authors: [@author])
+@book = Book.create(title: "The Pale King", authors: [@author])
+@book = Book.create(title: "Consider the Lobster", authors: [@author])
