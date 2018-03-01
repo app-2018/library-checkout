05c4568a554fccc8c04f4e3c9aba5c992d2cfe09
Karl Stolley <karl.stolley@gmail.com>
2018-03-01 12:48:26 -0600

    Reject nested author fields if last name is blank
    This uses the `reject_if:` option on `accepts_nested_attributes_for`,
    and references a custom `incomplete_author` method that checks to see
    whether the `last_name` attribute on any nested author fields is blank.
    
    According to the docs linked to in the code comment in this commit, it
    is possible to pass `:all_blank` to `reject_if:`, but for author records
    one assumes that a corporate author might have only the last name filled
    in, so it's better to write a custom method. And of course as author
    records get more complex, it would be easy to add additional checks to
    to the `incomplete_author` method.

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,5 +1,10 @@
 class Book < ApplicationRecord
   has_and_belongs_to_many :authors
   validates :title, presence: true
-  accepts_nested_attributes_for :authors
+  accepts_nested_attributes_for :authors, reject_if: :incomplete_author
+
+  # See http://api.rubyonrails.org/classes/ActiveRecord/NestedAttributes/ClassMethods.html
+  def incomplete_author(attributes)
+    attributes['last_name'].blank?
+  end
 end
